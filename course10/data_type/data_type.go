9e0928d7b236205643346ac6413ea14dddf60c5f
withong <withong_1314@163.com>
2020-04-16 16:09:04 +0800

    数据类型

--- course10/data_type/data_type.go
+++ course10/data_type/data_type.go
@@ -4,7 +4,7 @@
  * @Author: Winston
  * @Date: 2020-04-16 09:35:06
  * @LastEditors: Winston
- * @LastEditTime: 2020-04-16 11:37:06
+ * @LastEditTime: 2020-04-16 15:57:15
  */
 
 // 基本数据类型
@@ -112,6 +112,21 @@ var b := 'x'
 
 Go 使用了特殊的 rune 类型来处理 Unicode，让基于 Unicode 的文本处理更为方便，
 也可以使用 byte 型进行默认字符串处理，性能和扩展性都有照顾。
+
+修改字符串
+要修改字符串，需要先将其转换成[]rune或[]byte，完成后再转换为string。无论哪种转换，都会重新分配内存，并复制字节数组。
+*/
+
+/*
+类型转换
+强制类型转换的基本语法如下：
+T(表达式)
+其中，T表示要转换的类型。表达式包括变量、复杂算子和函数返回值等.
+比如计算直角三角形的斜边长时使用math包的Sqrt()函数，该函数接收的是float64类型的参数，
+而变量a和b都是int类型的，这个时候就需要将a和b强制类型转换为float64类型。
+
+
+
 */
 
 package main
@@ -143,6 +158,27 @@ func traversalString() {
 		字符串底层是一个byte数组，所以可以和[]byte类型相互转换。字符串是不能修改的 字符串是由byte字节组成，
 		所以字符串的长度是byte字节的长度。 rune类型用来表示utf8字符，一个rune字符由一个或多个byte组成。
 	*/
+}
+
+func changeString() { //字符串修改展示
+	s1 := "big"
+	// 强制类型转换
+	byteS1 := []byte(s1)
+	byteS1[0] = 'p'
+	fmt.Println(string(byteS1))
+
+	s2 := "白萝卜"
+	runeS2 := []rune(s2)
+	runeS2[0] = '红'
+	fmt.Println(string(runeS2))
+}
+
+func sqrtDemo() { //字符串转换示例
+	var a, b = 3, 4
+	var c int
+	// math.Sqrt()接收的参数是float64类型，需要强制转换
+	c = int(math.Sqrt(float64(a*a + b*b)))
+	fmt.Println(c)
 }
 
 func main() {
@@ -189,3 +225,7 @@ func main() {
 	`
 	fmt.Println(s1)
 }
+
+//练习：
+//编写代码分别定义一个整型、浮点型、布尔型、字符串型变量，使用fmt.Printf()搭配%T分别打印出上述变量的值和类型。
+//编写代码统计出字符串"hello大帅哥"中汉字的数量。
